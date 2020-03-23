ba56485d12972051ed2490bec61cd47e3ff0390a
guilhermeulbriki <guiulbriki@gmail.com>
2020-03-23 01:07:44 -0300

    api maps and go to forms

---
+++ maps.js
@@ -0,0 +1,42 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -27.3544219, lng: -53.3942224 },
+    zoom: 15,
+    styles: [{
+      "featureType": "poi",
+      "stylers": [{
+        "visibility": "off",
+      }]
+    }]
+  });
+
+  var iconBase = "./assets/images/points/";
+  var icons = {
+    hospital: {
+      name: "Hospital",
+      icon: iconBase + "blue-point.png"
+    },
+    Food: {
+      name: "Lancherias",
+      icon: iconBase + "yellow-point.png"
+    },
+    drugstore: {
+      name: "Farmácias",
+      icon: iconBase + "red-point.png"
+    }
+  };
+
+  const legend = document.querySelector("#legend");
+  for (var key in icons) {
+    let type = icons[key];
+    let name = type.name;
+    let icon = type.icon;
+    let span = document.createElement("span");
+    span.innerHTML = "<img src='" + icon + "'>" + name;
+    legend.appendChild(span);
+  }
+
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(legend);
+}
